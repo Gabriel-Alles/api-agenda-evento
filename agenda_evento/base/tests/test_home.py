dd93195064e287fd19ac729c5181b0a999234169
Gabriel Paniago Alles <gabriel.alles@hotmail.com>
2025-05-05 16:04:21 -0300

    Incluindo testes na função home

---
+++ agenda_evento/base/tests/test_home.py
@@ -0,0 +1,9 @@
+from django.test import Client
+
+def test_home_status(client: Client):
+    response = client.get('/base')
+    assert response.status_code == 200
+
+def test_home_status(client: Client):
+    response = client.get('/base')
+    assert response.content == b'Oi, cliente'    
